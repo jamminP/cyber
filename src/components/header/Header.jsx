a3f52728f87dd691ad4eb80a54e65608e7bc1700
jamminP <parkjaemin08@gmail.com>
2025-05-20 18:24:59 +0900

    ✨ Implement Mobile version Header
    모바일 버전 hedaer 생성 및 size에 맞게 화면에 수신되도록 구성

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,20 @@
 import './Header.css';
 import Desktop from './DesktopHeader';
+import Mobile from './MobileHeader';
 
 function Header(){
     return(
-        <div class="bg-white flex w-screen h-full px-40 py-4 items-center gap-8">
-            <Desktop></Desktop>
-        </div>
+        <header>
+            {/* Desktop version. (>= 768px) */}
+            <div class="bg-white w-screen h-full px-40 py-4 items-center gap-8 hidden md:flex">
+                <Desktop></Desktop>
+            </div>
+
+            {/* Mobile version. (< 768)*/}
+            <div class="bg-white w-screen h-full px-4 py-6 justify-between flex md:hidden">
+                <Mobile></Mobile>
+            </div>
+        </header>
     );
 }
 
---
+++ src/components/header/MobileHeader.jsx
@@ -0,0 +1,10 @@
+function MoblieHeader(){
+    return(
+        <>
+            <img src='/logo/Color=Black.png' alt="logo"></img>
+            <img src="/Icon/40px/Burger.png" alt="menu"></img>
+        </>
+    );
+}
+
+export default MoblieHeader;
