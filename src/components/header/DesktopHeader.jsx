d419846e37d84987b94a8931d48e6ebf07955096
jamminP <parkjaemin08@gmail.com>
2025-05-20 18:05:49 +0900

    feat(header): implement desktop header layout

---
+++ src/components/header/DesktopHeader.jsx
@@ -0,0 +1,33 @@
+function DesktopHeader(){
+
+    return (
+        <div class="flex items-center justify-center gap-6 w-full mx-auto max-w-[1440px]">
+            {/* logo */}
+            <img src='/logo/Color=Black.png' alt="logo"></img>
+
+            {/* 검색창 */}
+            <div class="rounded-lg p-4 gap-2 bg-[#F5F5F5] flex items-center w-full max-w-[433px]"> 
+                <img src="/Icon/24px/Search.png" alt="search"></img>
+                <input type="text" placeholder="Search" class="bg-[#F5F5F5] focus: outline-none w-full"
+                ></input>
+            </div>
+
+            {/* Button */}
+            <div class="flex gap-14">
+                <button>Home</button>
+                <button>About</button>
+                <button>Contact Us</button>
+                <button>Blog</button>
+            </div>
+
+            {/* Status */}
+            <div class="flex gap-6">
+                <img src="/Icon/32px/Favorites.png" alt="Favorites"></img>
+                <img src="Icon/32px/Cart.png" alt="cart"></img>
+                <img src="Icon/32px/User.png" alt="User"></img>
+            </div>
+        </div>
+    );
+}
+
+export default DesktopHeader;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,10 @@
 import './Header.css';
+import Desktop from './DesktopHeader';
 
 function Header(){
     return(
-        <div className='headerCompont'>
-            <p>Header</p>
+        <div class="bg-white flex w-screen h-full px-40 py-4 items-center gap-8">
+            <Desktop></Desktop>
         </div>
     );
 }
