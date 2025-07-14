b5df882c69e43e62195a1e9f0c9ff3ecc924847e
jamminP <parkjaemin08@gmail.com>
2025-07-14 18:37:31 +0900

    feat: BottomBanner 기능 구현

--- src/components/homes/BottomBanner.jsx
+++ src/components/homes/BottomBanner.jsx
@@ -1,5 +1,24 @@
 function BottomBanner() {
-  return <div>BottomBanner;</div>;
+  return (
+    <div className='relative h-[400px] w-full overflow-hidden'>
+      <img
+        src='/image/Banner_Sale.png'
+        alt='banner_image'
+        className='absolute inset-0 h-full w-full object-cover'
+      />
+      <div className='relative z-10 flex h-full w-full flex-col items-center justify-center px-4 text-center text-white'>
+        <h2 className='text-4xl font-thin md:text-6xl'>
+          Big Summer <span className='font-semibold'>Sale</span>
+        </h2>
+        <p className='mt-4 text-sm text-[#787878] md:text-base'>
+          Commodo fames vitae vitae leo mauris in. Eu consequat.
+        </p>
+        <button className='mt-6 rounded-[10px] border-2 border-white px-18 py-4.5 transition hover:bg-white hover:text-black'>
+          Shop Now
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default BottomBanner;
